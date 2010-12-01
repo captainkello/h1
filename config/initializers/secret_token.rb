8496338de38b047917fa50dba4171603d8da4b93
Capt. Kello <captainkello@gmail.com>
2010-12-01 09:59:07 -1000

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hpt::Application.config.secret_token = '8f86babd5c8f8855fabe3e7a74d958111e8dd6e13568ad67d73b3614fad5e347125c8dbb6f8fc6beb067456071ef1b52eb8b9eed03db2e62450faffdc174ef4e'
